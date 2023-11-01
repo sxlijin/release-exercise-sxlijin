43f59dba673c141d2e84678afe2e6abba0fbbc88
sam <github@sxlijin.com>
2023-11-01 09:36:06 -0600

    feat: get the controller built and running

--- internal/controller/helloworld_controller.go
+++ internal/controller/helloworld_controller.go
@@ -18,13 +18,17 @@ package controller
 
 import (
 	"context"
+	"fmt"
 
-	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	// Using `api` instead of `releaseexercisesourcegraphcomv1` for readability. Not sure what I would use in practice.
 	api "release-exercise/api/v1"
 )
 
@@ -48,20 +52,39 @@ type HelloWorldReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.0/pkg/reconcile
 func (r *HelloWorldReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
 
 	var helloworld api.HelloWorld
-	if err := r.client.Get(ctx, req.NamespacedName, &helloworld); err != nil {
-		log.Error(err, "unable to fetch HelloWorld resource")
+	if err := r.Client.Get(ctx, req.NamespacedName, &helloworld); err != nil {
+		logger.Error(err, "unable to fetch HelloWorld resource")
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
 	return ctrl.Result{}, nil
 }
 
+func createPod(hw *api.HelloWorld) *corev1.Pod {
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fmt.Sprintf("%s-pod", hw.Name),
+			Namespace: hw.Namespace,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:    "echo-container",
+					Image:   "ubuntu",
+					Command: []string{"/bin/echo", hw.Spec.Message},
+				},
+			},
+		},
+	}
+	return pod
+}
+
 // SetupWithManager sets up the controller with the Manager.
 func (r *HelloWorldReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&releaseexercisesourcegraphcomv1.HelloWorld{}).
+		For(&api.HelloWorld{}).
 		Complete(r)
 }
